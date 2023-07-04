f84832ead0fe316fe997d82026f6016fe1cec28e
TheCoolBoss <nguyenst22@up.edu>
2023-07-04 10:28:11 -0700

    Starting main class utils

--- src/main/java/com/solvd/block3/business_logic/Main.java
+++ src/main/java/com/solvd/block3/business_logic/Main.java
@@ -1,4 +1,21 @@
 package com.solvd.block3.business_logic;
 
-public class Main {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.solvd.block3.utilities.Utils;
+
+public class Main 
+{
+    private static final Logger LOGGER = LogManager.getLogger("Main");
+    public static void main(String[] args)
+    {
+        LOGGER.info("Welcome to the flight finder. Please input an origin city.");
+        String origin = Utils.makeCity();
+
+        LOGGER.info("Now input a destination.");
+        String destination = Utils.makeCity();
+
+        //For testing
+        LOGGER.info(origin + " " + destination);
+    }
 }
--- src/main/java/com/solvd/block3/utilities/SomeClass.java
+++
@@ -1,4 +0,0 @@
-package com.solvd.block3.utilities;
-
-public class SomeClass {
-}
