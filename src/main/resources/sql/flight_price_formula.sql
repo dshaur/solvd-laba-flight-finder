cccb01ce99cdf59b8dabc5f54a4c817ef8a5fb16
dshaur <shaur.david@gmail.com>
2023-07-04 16:36:19 -0400

    Created database and script files for the remote mysql server. Added the database schema, data insertion and updated flight prices using the Haversine formula for a realistic approach.

---
+++ src/main/resources/sql/flight_price_formula.sql
@@ -0,0 +1,6 @@
+use sz_flightfinder;
+
+UPDATE flights f
+JOIN airports a ON f.source_airport_id = a.airport_id
+JOIN airports b ON f.destination_airport_id = b.airport_id
+SET f.price = (ROUND(0.1 * (6371 * 2 * ASIN(SQRT(POWER(SIN((RADIANS(a.latitude) - RADIANS(b.latitude)) / 2), 2) + COS(RADIANS(a.latitude)) * COS(RADIANS(b.latitude)) * POWER(SIN((RADIANS(a.longitude) - RADIANS(b.longitude)) / 2), 2)))), 2) + 75);
