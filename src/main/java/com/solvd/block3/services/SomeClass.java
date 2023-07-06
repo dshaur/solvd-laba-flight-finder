ff8874c0e382ae37b02147a40d86cf20e4300487
TheCoolBoss <nguyenst22@up.edu>
2023-07-06 09:24:01 -0700

    Fixed joins

--- src/main/java/com/solvd/block3/services/SomeClass.java
+++ src/main/java/com/solvd/block3/services/SomeClass.java
@@ -3,30 +3,29 @@ package com.solvd.block3.services;
 import com.solvd.block3.models.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
 
 public class SomeClass {
 	private static final Logger LOGGER = LogManager.getLogger(SomeClass.class);
 	public static void main(String[] args) {
-		CountryServiceMyBatis cs = new CountryServiceMyBatis();
-		ArrayList<Country> c = cs.getCountries();
-		LOGGER.info(c);
+		// CountryServiceMyBatis cs = new CountryServiceMyBatis();
+		// ArrayList<Country> c = cs.getCountries();
+		// LOGGER.info(c);
 
-		CityServiceMyBatis ci = new CityServiceMyBatis();
-		City city = ci.getCity(4);
-		LOGGER.info(city);
+		// CityServiceMyBatis ci = new CityServiceMyBatis();
+		// City city = ci.getCity(4);
+		// LOGGER.info(city);
 
-		AirportServiceMyBatis as = new AirportServiceMyBatis();
-		Airport airport = as.getAirport(4);
-		LOGGER.info(airport);
+		// AirportServiceMyBatis as = new AirportServiceMyBatis();
+		// Airport airport = as.getAirport(4);
+		// LOGGER.info(airport);
 
-		AirlineServiceMyBatis asl = new AirlineServiceMyBatis();
-		Airline airline = asl.getAirline(2);
-		LOGGER.info(airline);
+		// AirlineServiceMyBatis asl = new AirlineServiceMyBatis();
+		// Airline airline = asl.getAirline(2);
+		// LOGGER.info(airline);
 
 		FlightServiceMyBatis fs = new FlightServiceMyBatis();
-		ArrayList<Flight> flights = fs.getFlightByAirline(1);
+		Flight flights = fs.getFlight(1);
 		LOGGER.info(flights);
 	}
 }
