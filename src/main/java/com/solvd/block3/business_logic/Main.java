72511cdef6fc4fbb30e0f51ff691ee91572e6cbb
TheCoolBoss <nguyenst22@up.edu>
2023-07-06 14:42:39 -0700

    UI now does stuff for the shortest route

--- src/main/java/com/solvd/block3/business_logic/Main.java
+++ src/main/java/com/solvd/block3/business_logic/Main.java
@@ -2,18 +2,13 @@ package com.solvd.block3.business_logic;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.solvd.block3.json.JsonMaker;
 import com.solvd.block3.models.*;
-import com.solvd.block3.services.AirportServiceMyBatis;
 import com.solvd.block3.ui.UiUtils;
-import com.solvd.block3.xml.XmlMaker;
 
 public class Main 
 {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
-    private static final String XML_PATH = "src/main/resources/Flights.xml";
-    private static final String JSON_PATH = "src/main/resources/Flights.json";
-    private static final AirportServiceMyBatis AIRPORT_SERVICE = new AirportServiceMyBatis();
+
     
     public static void main(String[] args) 
     {
@@ -23,7 +18,11 @@ public class Main
         LOGGER.info("Now input a destination.");
         City destination = UiUtils.makeCity();
 
-        Airport oriTest = AIRPORT_SERVICE.getAirportByCity(origin);
-        LOGGER.info(oriTest);
+        String mode = UiUtils.getMode();
+
+        if (mode.equalsIgnoreCase("s"))
+        {
+            UiUtils.makeShortestRoute(origin, destination);
+        }
     }
 }
--- src/main/java/com/solvd/block3/ui/UiUtils.java
+++ src/main/java/com/solvd/block3/ui/UiUtils.java
@@ -1,16 +1,49 @@
 package com.solvd.block3.ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.solvd.block3.json.JsonMaker;
+import com.solvd.block3.models.Airport;
 import com.solvd.block3.models.City;
+import com.solvd.block3.models.Flight;
+import com.solvd.block3.services.AirportServiceMyBatis;
 import com.solvd.block3.services.CityServiceMyBatis;
+import com.solvd.block3.services.FlightServiceMyBatis;
+import com.solvd.block3.xml.XmlMaker;
 
 public class UiUtils 
 {    
+    private static final String XML_PATH = "src/main/resources/Flights.xml";
+    private static final String JSON_PATH = "src/main/resources/Flights.json";
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Logger LOGGER = LogManager.getLogger(UiUtils.class);
     private static final CityServiceMyBatis CITY_SERVICE = new CityServiceMyBatis();
+    private static final AirportServiceMyBatis AIRPORT_SERVICE = new AirportServiceMyBatis();
+    private static final FlightServiceMyBatis FLIGHT_SERVICE = new FlightServiceMyBatis();
+
+    public static String getMode()
+    {
+        LOGGER.info("Enter 'c' for cheapest route, 's' for shortest route.");
+        String mode = SCANNER.nextLine();
+
+        while (true)
+        {
+            if (mode.equalsIgnoreCase("c") || mode.equalsIgnoreCase("s"))
+            {
+                return mode;
+            }
+
+            else
+            {
+                LOGGER.error("Invalid input.");
+                LOGGER.info("Enter 'c' for cheapest route, 's' for shortest route.");
+                mode = SCANNER.nextLine();
+            }
+        }
+    }
 
     public static City makeCity()
     {
@@ -25,5 +58,32 @@ public class UiUtils
         }
 
         return ret;
+    }
+
+    public static void makeShortestRoute(City source, City dest)
+    {
+        LOGGER.info("Getting data...");
+        Airport sourceAirport = AIRPORT_SERVICE.getAirportByCity(source);
+        Airport destAirport = AIRPORT_SERVICE.getAirportByCity(dest);
+        ArrayList<Flight> sourceFlights = FLIGHT_SERVICE.getFlightBySourceAirport(sourceAirport.getAirportId());
+        ArrayList<Flight> commonFlights = new ArrayList<Flight>();
+
+        sourceFlights.forEach(flight -> 
+        {   
+            //I should probably use the equals() method, but not too concerned with that right now
+            if (flight.getDestinationAirport().getAirportId() == destAirport.getAirportId())
+            {
+                commonFlights.add(flight);
+            }
+        });
+
+        LOGGER.info("The shortest route between " + source.getName() + " and " + dest.getName() + " is a direct route.");
+        LOGGER.info("Here is a list of possible flights.");
+        commonFlights.forEach(flight -> 
+        {   
+            LOGGER.info(flight);
+            XmlMaker.makeXml(XML_PATH, flight);
+            JsonMaker.makeJson(JSON_PATH, flight);
+        });
     }
 }
