3c36343f1cbe409e3555e8bd15c331f2ff55c1ca
dshaur <shaur.david@gmail.com>
2023-07-09 12:30:44 -0400

    Improved UiUtils to display the available cities origins and destinations to the user and get their input.

--- src/main/java/com/solvd/block3/business_logic/Main.java
+++ src/main/java/com/solvd/block3/business_logic/Main.java
@@ -1,26 +1,22 @@
 package com.solvd.block3.business_logic;
 
+import com.solvd.block3.ui.UiUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.solvd.block3.json.JsonMaker;
-import com.solvd.block3.ui.UiUtils;
-import com.solvd.block3.xml.XmlMaker;
 
-public class Main 
-{
+public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
     private static final String XML_PATH = "src/main/resources/Flights.xml";
     private static final String JSON_PATH = "src/main/resources/Flights.json";
-    
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         LOGGER.info("Welcome to the flight finder. Please input an origin city.");
-        String origin = UiUtils.makeCity();
+
+        String origin = UiUtils.selectCity();
+        LOGGER.info("Selected origin city: " + origin);
 
         LOGGER.info("Now input a destination.");
-        String destination = UiUtils.makeCity();
-
-        //For testing
-        LOGGER.info(origin + " " + destination);
+        String destination = UiUtils.selectCity();
+        LOGGER.info("Selected destination city: " + destination);
     }
 }
--- src/main/java/com/solvd/block3/ui/UiUtils.java
+++ src/main/java/com/solvd/block3/ui/UiUtils.java
@@ -1,32 +1,58 @@
 package com.solvd.block3.ui;
 
-import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class UiUtils 
-{    
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UiUtils {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Logger LOGGER = LogManager.getLogger(UiUtils.class);
+    private static final Map<String, String> cityMap = createCityMap();
 
-    public static String makeCity()
-    {
+    private static Map<String, String> createCityMap() {
+        Map<String, String> cityMap = new HashMap<>();
+        cityMap.put("1", "Seoul");
+        cityMap.put("2", "Denver");
+        cityMap.put("3", "London");
+        cityMap.put("4", "Dubai");
+        cityMap.put("5", "Sydney");
+        cityMap.put("6", "Lima");
+        cityMap.put("7", "Paris");
+        cityMap.put("8", "Berlin");
+        cityMap.put("9", "Beijing");
+        cityMap.put("10", "São Paulo");
+        cityMap.put("11", "Toronto");
+        cityMap.put("12", "Mumbai");
+        cityMap.put("13", "Moscow");
+        cityMap.put("14", "Amsterdam");
+        cityMap.put("15", "Mexico City");
+        cityMap.put("16", "Rome");
+        cityMap.put("17", "Madrid");
+        cityMap.put("18", "Istanbul");
+        cityMap.put("19", "Tokyo");
+        cityMap.put("20", "Cape Town");
+        return cityMap;
+    }
+
+    public static String selectCity() {
+        LOGGER.info("Available cities:");
+        for (Map.Entry<String, String> entry : cityMap.entrySet()) {
+            LOGGER.info(entry.getKey() + ": " + entry.getValue());
+        }
+
         String ret = SCANNER.nextLine();
-        while (!validateCity(ret))
-        {
-            LOGGER.error("City doesn't exist in database. Please try again.");
+        while (!validateCity(ret)) {
+            LOGGER.error("City doesn't exist in the database. Please try again.");
             ret = SCANNER.nextLine();
         }
 
-        return ret;
+        return cityMap.get(ret);
     }
 
-    public static boolean validateCity(String city)
-    {
-        boolean ret = true;
-        //Use mybatis to return a city object with the name parameter
-        //If null, return false;
-
-        return ret;
+    public static boolean validateCity(String cityId) {
+        return cityMap.containsKey(cityId);
     }
 }
